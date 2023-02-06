3a020dec2449a49eaf8aac90d354782a0ad82402
Maaz Karim <MaazKarim02@gmail.com>
2023-02-06 17:23:57 +0530

    Temp Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './css/footer.css'
 function Footer() {
   return (
-    <div></div>
+    <div className='footer'>
+      <div className='footer-text-div'>
+      <p className='footer-contact'>
+        Contact Us
+      </p>
+      <h1 className='footer-email'>
+        hackaventus@support.com
+      </h1>
+      </div>
+    </div>
   )
 }
 
