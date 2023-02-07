cc1cbc7c8be2784f1d7bf573fd61a600f70b7bf3
RahulSharma4329$ <rahulsharma4329@gmail.com>
2023-02-07 08:54:43 +0530

    2nd rev

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import "./css/landing.css"
 import { useEffect } from 'react'
-import { swarmBackground } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+import { swarmBackground } from 'threejs-toys'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Landing() {
 
@@ -15,7 +14,7 @@ function Landing() {
     const bg = swarmBackground({
       el: document.getElementById('effect'),
       eventsEl: document.getElementById('effect'),
-      gpgpuSize: 256,
+      gpgpuSize: 170,
       color: [0x00a69e, 0x00a69e],
       geometry: 'default'
     })
@@ -23,8 +22,10 @@ function Landing() {
     bg.setColors([0x00a69e, 0x00ff51])
     bg.three.camera.position.set(0, 0, 250)
 
-    let ele = document.querySelectorAll('[data-engine="three.js r140"]')
-    ele[0].style.display = "none"
+    let ele = document.querySelectorAll('canvas')
+    if (ele[0] !== undefined) {
+      ele[0].style.display = "none"
+    }
     if (ele[1] !== undefined) {
       ele[1].style.borderRadius = "20px"
     }
@@ -46,7 +47,7 @@ function Landing() {
           <div className="devbtn">
             <button className="dbtn">
               <div>Apply with Devfolio</div>
-              <svg class="logo" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 115.46 123.46" style={{ "height": "24px", "width": "24px", "margin-right": "8px" }}>
+              <svg className="logo" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 115.46 123.46" style={{ "height": "24px", "width": "24px", "marginRight": "8px" }}>
                 <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z"></path>
                 <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z"></path>
               </svg>
@@ -62,6 +63,11 @@ function Landing() {
             <button className="sbtn"><FontAwesomeIcon icon={brands('linkedin')} className={"fa-2x"} /></button>
           </div>
         </div>
+      </div>
+      <div className="presented">
+        <div className="ptext">brought to you by</div>
+        <div className="pclg">Dayananda Sagar College of Engineering</div>
+        <div className="pname">Department of Artificial Intelligence & Machine Learning </div>
       </div>
     </div>
   )
