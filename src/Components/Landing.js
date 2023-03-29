bd96ef2d0a02fe450d30d32c4a4511228f40cd29
Maaz Karim <MaazKarim02@gmail.com>
2023-03-29 16:23:41 +0530

    Added Devfolio button

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,7 +7,16 @@ import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Landing() {
 
-
+  useEffect(() => {
+      const script = document.createElement('script');
+      script.src = 'https://apply.devfolio.co/v2/sdk.js';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+        return () => {
+        document.body.removeChild(script);
+      }
+    }, []);
 
   useEffect(() => {
 
@@ -48,13 +57,20 @@ function Landing() {
           <div className="edate">5th & 6th May 2023</div>
           <div className="eline">{"24 Hours Hack"}</div>
           <div className="devbtn">
-            <button className="dbtn">
+            {/* <button className="dbtn">
               <div>Apply with Devfolio</div>
               <svg className="logo" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 115.46 123.46" style={{ "height": "24px", "width": "24px", "marginRight": "8px" }}>
                 <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z"></path>
                 <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z"></path>
               </svg>
-            </button></div>
+            </button> */}
+            <div 
+              className="apply-button" 
+              data-hackathon-slug="aventus" 
+              data-button-theme="dark"
+              style={{display:"flex", flexDirection:"column", zIndex:100}}
+            ></div>
+          </div>
         </div>
         <div className="lright">
           <div className="llogo"><img src="logo.png" alt="" /></div>
