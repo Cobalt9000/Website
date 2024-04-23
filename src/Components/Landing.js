63f21b83e048fea50fa1611159a8f36bfb956862
Gurudarshan57 <venom573116@gmail.com>
2024-04-23 08:22:19 +0530

    Fixed home page mobile view

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -8,32 +8,32 @@ import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 function Landing() {
   // const [height, setHeight] = useState('100%');
   useEffect(() => {
-      const script = document.createElement('script');
-      script.src = 'https://apply.devfolio.co/v2/sdk.js';
-      script.async = true;
-      script.defer = true;
-      document.body.appendChild(script);
-        return () => {
-        document.body.removeChild(script);
-      }
-    }, []);
-    
-    // useEffect(() => {
-    //   var landing_main = document.getElementById('landing-main');
-    //   if(landing_main.offsetHeight) {
-    //     setHeight(landing_main.offsetHeight);
-    
-    //   } else if(landing_main.style.pixelHeight) {
-    //       setHeight(landing_main.style.pixelHeight);
-      
-    //   }
-    // console.log({height});
-    // const divEffect = document.getElementById('effect');
-    // divEffect.style.height = height;
-    // console.log({divEffect});
-    // }, [height] )
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
-    useEffect(() => {
+  // useEffect(() => {
+  //   var landing_main = document.getElementById('landing-main');
+  //   if(landing_main.offsetHeight) {
+  //     setHeight(landing_main.offsetHeight);
+
+  //   } else if(landing_main.style.pixelHeight) {
+  //       setHeight(landing_main.style.pixelHeight);
+
+  //   }
+  // console.log({height});
+  // const divEffect = document.getElementById('effect');
+  // divEffect.style.height = height;
+  // console.log({divEffect});
+  // }, [height] )
+
+  useEffect(() => {
     const bg = swarmBackground({
       el: document.getElementById('effect'),
       eventsEl: document.getElementById('effect'),
@@ -41,13 +41,13 @@ function Landing() {
       color: [0x00a69e, 0x00a69e],
       geometry: 'default'
     })
-    
+
     bg.setColors([0x00a69e, 0x00ff51])
     bg.three.camera.position.set(0, 0, 250)
-    
+
     let ele = document.querySelectorAll('canvas')
     if (ele.length > 1) {
-      
+
       if (ele[0] !== undefined) {
         ele[0].style.display = "none"
       }
@@ -55,10 +55,10 @@ function Landing() {
     ele.forEach((e) => {
       e.style.borderRadius = "20px"
     })
-    
+
   }, [])
-  
-  
+
+
 
 
   return (
@@ -67,35 +67,35 @@ function Landing() {
       </div>
       <div className="lcontent">
         <div className="lleft">
-          <div className="ename">Aventus 2.0</div>
+          <div className="ename"><div>AVENTUS</div><div className='name_two'>2.0</div></div>
           <div className="etagline">where technology flourishes</div>
           <div className="etagline">#wtf</div>
           <div className="edate">18th - 19th May 2024</div>
           <div className="eline">{"24 Hour Hackathon"}</div>
           <div className="devbtn">
-            <div class="apply-button" 
-	            data-hackathon-slug="aventus-2-0" 
-	            data-button-theme="dark"
-	            style={{display:"flex", flexDirection:"column", zIndex:100}}
-              >
-            </div> 
+            <div class="apply-button"
+              data-hackathon-slug="aventus-2-0"
+              data-button-theme="dark"
+              style={{ display: "flex", flexDirection: "column", zIndex: 100 }}
+            >
+            </div>
             <a href=''>
               <button className="dbtn">
-              <p>Register Now</p>
-              {/* <svg className="logo" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 115.46 123.46" style={{ "height": "24px", "width": "24px", "marginRight": "8px" }}>
+                <p>Register Now</p>
+                {/* <svg className="logo" xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 115.46 123.46" style={{ "height": "24px", "width": "24px", "marginRight": "8px" }}>
                 <path d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.05 1.05.08 2.18.08 3.34z"></path>
                 <path d="M110.93 55.87A55.43 55.43 0 0 1 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z"></path>
               </svg> */}
-            </button>
-                 
+              </button>
+
             </a>
-            
+
             <a href=''>
               <button className="timeline">
-              <p>Timeline</p>
-            </button>
+                <p>Timeline</p>
+              </button>
             </a>
-            <div className="p-10" style={{display:"flex", flexDirection:"column", zIndex:100}}>
+            <div className="p-10" style={{ display: "flex", flexDirection: "column", zIndex: 100 }}>
               {/* <div
                   className="apply-button"
                   data-hackathon-slug="aventus-2-0"
