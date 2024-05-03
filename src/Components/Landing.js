4a423484d09981872ab6348ac24467fea187f787
Cob@lt9000 <suprith1201@gmail.com>
2024-05-03 08:06:52 +0530

    adding the discord icon and the link

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -109,11 +109,15 @@ function Landing() {
           <div className="msg">Connect with us on our socials</div>
           <div className="socialmediabtn">
             <a href="https://www.instagram.com/hackaventus/" target="_blank" rel="noopener noreferrer">
-              <button className="sbtn"><FontAwesomeIcon icon={brands('instagram')} className={"fa-2x"} /></button>
+              <button className="sbtn"><FontAwesomeIcon icon={brands('instagram')} className={"fa-3x"} /></button>
             </a>
 
             <a href="https://www.linkedin.com/showcase/aventus-dsce/" target="_blank" rel="noopener noreferrer">
-              <button className="sbtn"><FontAwesomeIcon icon={brands('linkedin')} className={"fa-2x"} /></button>
+              <button className="sbtn"><FontAwesomeIcon icon={brands('linkedin')} className={"fa-3x"} /></button>
+            </a>
+
+            <a href="https://discord.com/invite/Mbb9bkdsCj" target="_blank" rel="noopener noreferrer">
+              <button className="sbtn"><FontAwesomeIcon icon={brands('discord')} className={"fa-3x"} /></button>
             </a>
           </div>
         </div>
